bdafdb83eda9417fc04aa8b660f9e5b5e8c037d5
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-09-30 22:35:58 +0800

    创建第三次和第四次作业的骨架

---
+++ 03/app/src/main/java/com/example/a03/Frist.java
@@ -0,0 +1,15 @@
+package com.example.a03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frist);
+    }
+    
+}
