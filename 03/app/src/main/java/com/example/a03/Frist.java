1ece68b80ea352595ecc3741403087a8a6401f1d
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-09-30 22:59:19 +0800

    完成到85页

--- 03/app/src/main/java/com/example/a03/Frist.java
+++ 03/app/src/main/java/com/example/a03/Frist.java
@@ -3,13 +3,27 @@ package com.example.a03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class Frist extends AppCompatActivity {
+public class Frist extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frist);
     }
-    
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+            default:
+                break;
+
+
+        }
+    }
 }
