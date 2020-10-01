c3b9e4a2f93262a53900082182faf263d85b89aa
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-01 14:56:08 +0800

    完成到p91页

--- 03/app/src/main/java/com/example/a03/Frist.java
+++ 03/app/src/main/java/com/example/a03/Frist.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class Frist extends AppCompatActivity  implements View.OnClickListener{
-
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frist);
+        Button button=(Button)findViewById(R.id.button1);
+        progressBar=(ProgressBar)findViewById(R.id.progress_bar);
+        button.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button1:
+                if(progressBar.getVisibility()==View.GONE){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else{
+                    progressBar.setVisibility(View.GONE);
+            }
                 break;
             case R.id.button2:
                 break;
