78b70126063ebdc8a4527b3be244f591efe1a0f6
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-06 12:08:24 +0800

    完成书本progressbar练习

--- 03/app/src/main/java/com/example/a03/Frist.java
+++ 03/app/src/main/java/com/example/a03/Frist.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class Frist extends AppCompatActivity  implements View.OnClickListener{
     private ProgressBar progressBar;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frist);
         Button button=(Button)findViewById(R.id.button1);
-        progressBar=(ProgressBar)findViewById(R.id.progress_bar);
+        //progressBar=(ProgressBar)findViewById(R.id.progress_bar);
+        imageView=(ImageView)findViewById(R.id.image2);
         button.setOnClickListener(this);
 
     }
@@ -23,14 +26,10 @@ public class Frist extends AppCompatActivity  implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button1:
-                if(progressBar.getVisibility()==View.GONE){
-                    progressBar.setVisibility(View.VISIBLE);
-                }
-                else{
-                    progressBar.setVisibility(View.GONE);
-            }
+                imageView.setImageResource(R.drawable.ab);
                 break;
             case R.id.button2:
+
                 break;
             default:
                 break;
