4a5515228470e1923c08dc651adaec67b625dc71
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-06 20:52:51 +0800

    完成线性布局练习

--- 03/app/src/main/java/com/example/a03/Frist.java
+++ 03/app/src/main/java/com/example/a03/Frist.java
@@ -2,6 +2,8 @@ package com.example.a03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,19 +18,41 @@ public class Frist extends AppCompatActivity  implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frist);
         Button button=(Button)findViewById(R.id.button1);
+        Button button1=(Button)findViewById(R.id.button2);
         //progressBar=(ProgressBar)findViewById(R.id.progress_bar);
         imageView=(ImageView)findViewById(R.id.image2);
         button.setOnClickListener(this);
+        button1.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+
             case R.id.button1:
                 imageView.setImageResource(R.drawable.ab);
                 break;
             case R.id.button2:
+                System.out.println("hello");
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(Frist.this);
+                alertDialog.setTitle("对话框弹出");
+                alertDialog.setMessage("是否前往百度");
+                alertDialog.setCancelable(false);
+                alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                                 
+                    }
+                });
+                alertDialog.setNegativeButton("cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDialog.show();
+
 
                 break;
             default:
