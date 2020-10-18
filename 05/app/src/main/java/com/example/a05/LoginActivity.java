43cfb67cf6781b46667210e1122041fc21bc2d8a
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-18 14:47:38 +0800

    完成登陆界面功能

---
+++ 05/app/src/main/java/com/example/a05/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.a05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Login{
+    private EditText account_editText;
+    private EditText password_editText;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        account_editText=(EditText)findViewById(R.id.account);
+        password_editText=(EditText)findViewById(R.id.password);
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = account_editText.getText().toString();
+                String password = password_editText.getText().toString();
+                if(account.equals("lin")&&password.equals("123456")){
+                    Intent intent=new Intent(LoginActivity.this,MysqlTest.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"密码或用户名错误",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
