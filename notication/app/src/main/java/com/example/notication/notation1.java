a1b9bc05e2090ecb6830390c7a858b90fa912f84
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-26 20:48:41 +0800

    着手编写手机通知功能

---
+++ notication/app/src/main/java/com/example/notication/notation1.java
@@ -0,0 +1,14 @@
+package com.example.notication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notation1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notication1);
+    }
+}
