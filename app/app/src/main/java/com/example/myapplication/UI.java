ed5e350c84bcde3c4c417f0600755a38d06f4609
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-11-28 11:05:56 +0800

    实现滑动菜单

--- app/app/src/main/java/com/example/myapplication/UI.java
+++ app/app/src/main/java/com/example/myapplication/UI.java
@@ -1,14 +1,58 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class UI extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_i);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        //setActionBar(toolbar);nB
+        ActionBar actionBar=getSupportActionBar();
+        drawerLayout=findViewById(R.id.drawer);
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"后退",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"删除",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"设置",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+            default:
+
+        }
+        return true;
     }
 }
