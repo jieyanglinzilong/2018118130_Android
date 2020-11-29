21ff73ffaac208b4ba5d9d3988c5dc3306fc0302
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-11-29 15:05:12 +0800

    实现floatbutton

--- app/app/src/main/java/com/example/myapplication/UI.java
+++ app/app/src/main/java/com/example/myapplication/UI.java
@@ -10,8 +10,13 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class UI extends AppCompatActivity {
     private DrawerLayout drawerLayout;
@@ -21,13 +26,37 @@ public class UI extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_i);
         Toolbar toolbar=findViewById(R.id.toolbar);
+
         //setActionBar(toolbar);nB
         ActionBar actionBar=getSupportActionBar();
+        NavigationView navigationView=findViewById(R.id.nav_view);
         drawerLayout=findViewById(R.id.drawer);
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+        navigationView.setCheckedItem(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        FloatingActionButton floatingActionButton=findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Toast.makeText(UI.this,"是否提交",Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,"是否提交",Snackbar.LENGTH_SHORT).setAction("提交",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(UI.this,"保存",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
 
     }
     @Override
